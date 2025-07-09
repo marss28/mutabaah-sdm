3cc180832c68c9f525638bdb415f3dde5aa1f2d7
marss28 <marsyapuspa9@gmail.com>
2025-07-09 15:09:55 +0700

    logout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BannerinfoController;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -12,9 +14,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // BANNER INFO
+    Route::get('/bannerinfo', [BannerinfoController::class, 'index'])->name('bannerinfo');
+    Route::get('/formbannerinfo', [BannerinfoController::class, 'formbannerinfo'])->name('formbannerinfo');
 });
 
 require __DIR__.'/auth.php';
